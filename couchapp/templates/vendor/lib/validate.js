0bbc6e1a9d18c4eac17ded7f2803fd67d79dff19
Teddy Caddy <info@teddycaddy.com>
2011-03-23 11:58:11 -0400

    changes to validate.js:   * add v.isDbAdmin() function to test if user is an admin     for a given database.   * extend v.isAdmin() to accept optional argument, which will     also check v.isDbAdmin() before returning false.

--- couchapp/templates/vendor/lib/validate.js
+++ couchapp/templates/vendor/lib/validate.js
@@ -15,9 +15,51 @@ exports.init = function(newDoc, oldDoc, userCtx, secObj) {
     if (!should) v.forbidden(message);
   }
   
-  v.isAdmin = function() {
-    return userCtx.roles.indexOf('_admin') != -1
+  v.isAdmin = function(dbAdminCheck) {
+    /*
+      Changes from fork by tcaddy:
+        Extend this function to take an optional argument called dbAdminCheck.
+        If dbAdminCheck is true and user is not a real admin, we will 
+        return the result of v.isDbAdmin()
+    */
+    if (typeof(dbAdminCheck) === 'undefined') {
+      var dbAdminCheck = false; // set to false, by default
+    }
+    var is_admin = (userCtx.roles.indexOf('_admin') != -1);
+    if (is_admin) {
+      return true; // real admins don't need additional checks
+    } else if(dbAdminCheck) {
+      return v.isDbAdmin(); // this will check if user is an admin on given database
+    }
+    return false; // return false by default
   };
+  
+  v.isDbAdmin() = function() {
+    /*
+      Changes from fork by tcaddy:
+        Futon lets you define specific username and/or roles that act
+        as admins for a single database.  But there is no check for this.
+        
+        This function will perform the check for us.
+    */
+    if (typeof(secObj) !== 'undefined') { // if secObj isn't defined, we do nothing
+      if (secObj && secObj.admins) { // if secObj.admins isn't defined, we do nothing
+        if (secObj.admins.names) {
+          if (secObj.admins.names.indexOf(userCtx.name) != -1) {
+            return true; // username matches
+          }
+        }
+        if (secObj.admins.roles) {
+          for (var i=0, l=secObj.admins.roles.length; i<l; i++) {
+            if (v.isRole(secObj.admins.roles[i])) {
+              return true; // role matches
+            }
+          }
+        }
+      }
+    }
+    return false; // return false by default
+  }
   
   v.isRole = function(role) {
     return userCtx.roles.indexOf(role) != -1
